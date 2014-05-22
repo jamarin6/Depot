5bfc4889f78fa67b37e2bf0abcb1b397f0ab3bee
jamarin6 <jamarinhidalgo@gmail.com>
2014-05-22 17:45:18 +0000

    new validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,10 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
-	validates :title, uniqueness: true
+	validates :title, uniqueness: true,
+					  length: { minimum: 10 }
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	#validates_format_of :image_url, allow_blank: true,
-	#	:with =>     %r{\.(gif|jpg|png)}i,
-	#	:message =>  'must be a URL for GIF, JPG or PNG image.'
-    
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}   # *****ESTO NO VALE como viene en el libro, he borrado $ aqui -> png)$}i, y va ok*****
-
-
 end

c343361715b8ec5d5b04bcc05c6dbd084d13a130
jamarin6 <jamarinhidalgo@gmail.com>
2014-06-02 11:40:38 +0000

    introducing cart & line_item

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
+
+  private
+
+	 def current_cart
+	   Cart.find(session[:cart_id])      # encuentra el id del carro
+	 rescue ActiveRecord::RecordNotFound # y si no lo encuentra...
+	   cart = Cart.create                # crea un nuevo carro "cart"
+	   session[:cart_id] = cart.id       # y al id de la sesion le mete el id del carro creado
+	   cart                              # y al final devuelve el carro creado
+	 end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create(title: 'CoffeeScript',
+Product.create(title: 'CoffeeScript3',
   description: 
     %{<p>
         CoffeeScript is JavaScript done right. It provides all of JavaScript's
@@ -28,7 +28,7 @@ Product.create(title: 'CoffeeScript',
   image_url:   'cs.jpg',    
   price: 36.00)
 # . . .
-Product.create(title: 'Programming Ruby 1.9',
+Product.create(title: 'Programming Ruby 1.93',
   description:
     %{<p>
         Ruby is the fastest growing and most exciting dynamic language
@@ -39,7 +39,7 @@ Product.create(title: 'Programming Ruby 1.9',
   price: 49.95)
 # . . .
 
-Product.create(title: 'Rails Test Prescriptions',
+Product.create(title: 'Rails Test Prescriptions3',
   description: 
     %{<p>
         <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
